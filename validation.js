e1a20edc1be0cb6805a2b2b60519b853f8672b0b
Aditya Kumar <143181831+adityakrsahu@users.noreply.github.com>
2023-10-07 16:13:35 +0530

    add

--- validation.js
+++ validation.js
@@ -1,10 +1,13 @@
 function validation(){
     let nam = document.getElementById('nam').value;
     let username = document. getElementById ('username').value;
-    let mob = document . getElementById ('mob').value;
+    let mob =parseInt( document . getElementById ('mob').value);
+    let  maxmob =document.getElementById('mob').value;
+    console.log(typeof(mob))
+    console.log(typeof(mob))
     let pass = document.getElementById ('pass').value;
     let cpass = document.getElementById ('cpass').value;
-    console.log(nam,username,mob,pass,cpass);
+    //console.log(nam,username,mob,pass,cpass);
 
     if(nam === ''){
         alert("please emter name")
@@ -20,20 +23,34 @@ function validation(){
         alert("@gmail.com must be include")
         return false;
     }
-    else if(mob === ''){
+    else if(isNaN(mob)){
         alert("please emter mobile")
         document.getElementById('mob').focus()
         return false
     }
+    else if (maxmob.length !==10){
+        alert("please enter valid mobile ")
+        return false;
+    }
+   
     else if(pass === ''){
         alert("please emter pass")
         document.getElementById('pass').focus()
         return false
     }
+    else if (! pass.includes('@')){
+        alert("@ must be include");
+        return false;
+    }
+
     else if(cpass === ''){
-        alert("please emter cpass")
+        alert("please emter cpass");
         document.getElementById('cpass').focus()
         return false
+    }
+    else if ( pass === cpass){
+        alert("password not matched");
+        return false;
     }
 
 }
